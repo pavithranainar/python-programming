fb63684e63d9977916e8ebc871d7f4f6c6b017f8
pavithranainar <43063219+pavithranainar@users.noreply.github.com>
2018-09-19 15:31:42 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,8 @@
+Num=float(input('Enter the third number:'))
+if(num1>num2)and (num1>num30
+  largest=num1
+elif(num2>num1)and(num2>num3)
+  largest=num2
+else:  
+  largest=num3
+ print('the largest number is num3')
