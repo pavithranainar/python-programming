fb332f4f70f9159b18e071bfe12eb706f9cb3da9
pavithranainar <43063219+pavithranainar@users.noreply.github.com>
2018-09-24 17:51:11 +0530

    Create even numbers two intervals.py

---
+++ even numbers two intervals.py
@@ -0,0 +1,8 @@
+print("DISPLAY EVEN NUMBERS BETWEEN 2 INTERVAL")
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+forxin range(a+1,):
+if(x%==0):
+print(x):
+else:
+    print("invalid")
