9cc75d0eac3bfb3df3400fe91d952673a5507788
pavithranainar <43063219+pavithranainar@users.noreply.github.com>
2018-09-25 16:29:15 +0530

    Create 2numbers(intervals).py

---
+++ 2numbers(intervals).py
@@ -0,0 +1,10 @@
+print("DISPLAY ODD NUMBERS BETWEEN 2 INTERVALS")
+a=int(input("enter the numbers"))
+b=int(input("enter the numbers"))
+if(a<=100000)(b<=10000):
+for x in range( a,b+1)
+if(x%2!=0):
+print(x)
+else:
+    print("invalid")
+    
