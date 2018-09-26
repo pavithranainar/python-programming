d130b950f0cd8ba3baa5f6c58abf0bdb47db783c
pavithranainar <43063219+pavithranainar@users.noreply.github.com>
2018-09-26 16:34:02 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,14 @@
+print("TO CHECK WHETHER A NUMBER IS ARMSTRONG")
+n=int(input("enter the number"))
+t=n
+s=0
+a=0
+while n>0:
+re=n%10
+a=re**3
+s=s+a
+n=n//0
+if(t==s)
+    print("armstrong"
+else:
+   print("not armstrong")
