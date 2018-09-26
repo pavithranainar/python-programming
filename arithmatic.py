d7a496bdcebbb2ea935dc393150530ff684b74f6
pavithranainar <43063219+pavithranainar@users.noreply.github.com>
2018-09-26 17:25:25 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,11 @@
+a=int(input("enter first number of an A.P series::"))
+n=int(input("enter the total number in this A. P series::"))
+d=int(input("enter the common difference:""))
+total=0 
+value=a
+print("arithmatic progression series:"end="")
+for 1 in range(n);
+print(%d+%value,end="")
+total=total+value
+value=value+d
+print("/n the sum of arithmatic progression series up to %=%d"%(n,total))
